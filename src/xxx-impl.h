463622a1e2eaeae2170b0f624769144456af9765
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-06-30 14:31:58 -0500

    Control visibility of symbols

--- src/xxx-impl.h
+++ src/xxx-impl.h
@@ -7,23 +7,15 @@
 #include <time.h>
 #include <xxx.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 struct xxx_t {
   unsigned verbose;
 };
 
-// Dynamic memory allocation function.
+// Dynamic memory allocation macro.
 #define xxx_calloc(T, n) ((T *)calloc(n, sizeof(T)))
 
-// Dynamic memory deallocation function.
-void xxx_free_(void **p);
+// Dynamic memory deallocation macro.
+XXX_INTERN void xxx_free_(void **p);
 #define xxx_free(p) xxx_free_((void **)p)
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // __LIBXXX_IMPL_H__
--- src/xxx.h
+++ src/xxx.h
@@ -1,17 +1,31 @@
 #if !defined(__LIBXXX_H__)
 #define __LIBXXX_H__
 
+#define XXX_VISIBILITY(mode) __attribute__((visibility(#mode)))
+
+#if defined(__cplusplus)
+#define XXX_EXTERN extern "C" XXX_VISIBILITY(default)
+#else
+#define XXX_EXTERN extern XXX_VISIBILITY(default)
+#endif
+
+#if defined(__cplusplus)
+#define XXX_INTERN extern "C" XXX_VISIBILITY(hidden)
+#else
+#define XXX_INTERN extern XXX_VISIBILITY(hidden)
+#endif
+
 struct xxx_t;
-struct xxx_t *xxx_init(int *argc, char **argv[]);
+XXX_EXTERN struct xxx_t *xxx_init(int *argc, char **argv[]);
 
-void xxx_debug(const int verbose, const char *fmt, ...);
+XXX_EXTERN void xxx_debug(const int verbose, const char *fmt, ...);
 
-void xxx_error(const char *fmt, ...);
+XXX_EXTERN void xxx_error(const char *fmt, ...);
 
-void xxx_assert_(int cond, const char *fmt, const char *file,
-                 const unsigned line);
+XXX_INTERN void xxx_assert_(int cond, const char *fmt, const char *file,
+                            const unsigned line);
 #define xxx_assert(COND, MSG) xxx_assert_(COND, MSG, __FILE__, __LINE__)
 
-void xxx_finalize(struct xxx_t **xxx);
+XXX_EXTERN void xxx_finalize(struct xxx_t **xxx);
 
 #endif // __LIBXXX_H__
