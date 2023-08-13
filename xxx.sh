c690166c584255de518a5140afa9f8ff5521f6e2
Thilina Ratnayaka <thilinarmtb@gmail.com>
2023-08-13 16:07:14 -0500

    Add AddressSanitizer if the build is a Debug build

--- xxx.sh
+++ xxx.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-: ${XXX_INSTALL_DIR:=`pwd`/install}
 : ${XXX_CC:=cc}
-: ${XXX_ENABLE_DOCS:=ON}
+: ${XXX_ENABLE_DOCS:=OFF}
+: ${XXX_BUILD_TYPE:=Debug}
+: ${XXX_INSTALL_PREFIX:=`pwd`/install}
 
 ### Don't touch anything that follows this line. ###
 XXX_CURRENT_DIR=`pwd`
@@ -11,9 +12,10 @@ XXX_BUILD_DIR=${XXX_CURRENT_DIR}/build
 mkdir -p ${XXX_BUILD_DIR} 2> /dev/null
 
 cmake -DCMAKE_C_COMPILER=${XXX_CC} \
-  -DCMAKE_INSTALL_PREFIX=${XXX_INSTALL_DIR} \
-  -DENABLE_DOCS=${XXX_ENABLE_DOCS} \
+  -DCMAKE_INSTALL_PREFIX=${XXX_INSTALL_PREFIX} \
+  -DCMAKE_BUILD_TYPE=${XXX_BUILD_TYPE} \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DENABLE_DOCS=${XXX_ENABLE_DOCS} \
   -B ${XXX_BUILD_DIR} -S ${XXX_CURRENT_DIR}
   
 cmake --build ${XXX_BUILD_DIR} --target install -j4
