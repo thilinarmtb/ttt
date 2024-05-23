0b7b8b4ac3d30b7629c4ddeb3ab5698705ca9df6
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-05-23 01:49:42 -0500

    Use the new API in tests and add ttt_test_check()

--- tests/ttt-000-init.c
+++ tests/ttt-000-init.c
@@ -4,11 +4,11 @@
 
 int main(int argc, char **argv) {
   // ttt_init() must succeed.
-  ttt_t ttt = ttt_init(&argc, &argv);
-  ttt_test_assert(ttt != NULL);
+  ttt_t ttt;
+  ttt_test_check(ttt_init(&ttt, &argc, &argv));
 
   // ttt_finalize() must succeed.
-  ttt_test_assert(ttt_finalize(&ttt) == TTT_SUCCESS);
+  ttt_test_check(ttt_finalize(&ttt));
 
   // Finalizing again should return invalid input error.
   ttt_test_assert(ttt_finalize(&ttt) == TTT_INVALID_USER_INPUT);
